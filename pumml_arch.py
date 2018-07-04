596dcadc97191d62ae70e26587b2f9f709c53840
Patrick Komiske <pkomiske@gmail.com>
2018-07-04 16:32:17 -0400

    added loss function

--- pumml_arch.py
+++ pumml_arch.py
@@ -1,11 +1,32 @@
-from keras.models import Sequential
+##========================================================================##
+##  File: pumml_arch.py                                                   ##
+##                                                                        ##
+##  Copyright (c) 2016-2018 Patrick Komiske, Eric Metodiev                ##
+##                                                                        ##
+##  This program is free software; you can redistribute it and/or modify  ##
+##  it under the terms of the GNU General Public License as published by  ##
+##  the Free Software Foundation; either version 3 of the License, or     ##
+##  (at your option) any later version.                                   ##
+##                                                                        ##
+##  This program is distributed in the hope that it will be useful,       ##
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of        ##
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         ##
+##  GNU General Public License for more details.                          ##
+##                                                                        ##
+##  You should have received a copy of the GNU General Public License     ##
+##  along with this program; If not, see <http://www.gnu.org/licenses/>.  ##
+##========================================================================##
+
+
+from keras import backend as K
 from keras.layers import Conv2D, LocallyConnected2D, ZeroPadding2D
+from keras.models import Sequential
 from keras.optimizers import Adam
+
 
 def parg(arg):
 
-    """ Useful function for making single elements into a one-element list,
-    i.e. for easy iteration. """
+    """Function for making single elements into a one-element list for iteration."""
 
     if type(arg) != list:
         return [arg]
@@ -13,37 +34,56 @@ def parg(arg):
         return arg 
 
 
-# function taking a hyperparameter dictionary (some examples are below)
+# constant appearing in PUMML loss function 
+pbar = 10.0
+
+def pumml_loss(y_true, y_pred):
+
+    """Custom per-pixel logarithmic squared loss function."""
+
+    first_log = K.log(K.clip(y_pred, K.epsilon(), None) + pbar)
+    second_log = K.log(K.clip(y_true, K.epsilon(), None) + pbar)
+    return K.mean(K.square(first_log - second_log), axis=-1)
+
+
 def pileup_model(hps):
 
-    img_size     =  hps['img_size']
-    nb_channels  =  hps['nb_channels']
-    
-    filter_size  =  parg(hps['filter_size'])
-    nb_filters   =  parg(hps['nb_filters'])
-    stride       =  parg(hps['stride'])
-    layers       =  parg(hps['layers'])
-    zero_pad     =  parg(hps.setdefault('zero_pad', stride[0]))
-    lr           =  hps.setdefault('lr', .001)
-    summary      =  hps.setdefault('summary', True)
+    """Function that takes a hyperparameter dictionary and returns a 
+    compiled Keras model.
+    """
 
-    proj_layer   =  hps.setdefault('proj_layer', Conv2D)
+    img_size     = hps['img_size']
+    nb_channels  = hps['nb_channels']
+    
+    filter_size  = parg(hps['filter_size'])
+    nb_filters   = parg(hps['nb_filters'])
+    stride       = parg(hps['stride'])
+    layers       = parg(hps['layers'])
+    zero_pad     = parg(hps.get('zero_pad', stride[0]))
+    lr           = hps.get('lr', .001)
+    summary      = hps.get('summary', True)
+
+    proj_layer   = hps.get('proj_layer', Conv2D)
+    loss         = hps.get('loss', pumml_loss)
  
     model = Sequential()
     
     for i in range(len(layers)):
+        opts = {'padding': (zero_pad[i], zero_pad[i])}
         if i == 0:
-            model.add(ZeroPadding2D(padding=(zero_pad[i], zero_pad[i]), input_shape=(nb_channels, img_size, img_size)))
-        else:
-            model.add(ZeroPadding2D(padding=(zero_pad[i], zero_pad[i])))
+            opts['input_shape'] = (nb_channels, img_size, img_size)
+        model.add(ZeroPadding2D(**opts))
+        
         model.add(layers[i](nb_filters[i], filter_size[i], activation='relu', strides=stride[i], kernel_initializer='he_uniform', padding='valid'))
 
     if nb_filters[len(layers)-1] > 1:
         model.add(proj_layer(1, 1, kernel_initializer='he_uniform', padding='valid', activation='relu'))
 
-    model.compile(loss=hps['loss'], optimizer=Adam(lr=lr))
+    model.compile(loss=loss, optimizer=Adam(lr=lr))
+
     if summary:
         model.summary()
+
     return model
 
 """hps_examples = {
